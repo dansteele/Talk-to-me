39b7fe22b56b5b794f38793ec91933f3e8fa3ef3
dansteele <danielsteele@hotmail.co.uk>
2015-02-03 10:03:24 +0000

    Started creating new user connection notification. Changed indentations

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', function(req, res){
 
 
 io.on('connection', function(socket){
+  io.emit('new user')
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
